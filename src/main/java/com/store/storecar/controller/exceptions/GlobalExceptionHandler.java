aa04d342b5f576adba62a237a27c969855ed884e
Jessica <rsp.assistencia@gmail.com>
2023-06-05 10:13:30 -0300

    error handler

--- src/main/java/com/store/storecar/controller/exceptions/GlobalExceptionHandler.java
+++ src/main/java/com/store/storecar/controller/exceptions/GlobalExceptionHandler.java
@@ -4,11 +4,15 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.NoHandlerFoundException;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
 @ControllerAdvice
@@ -19,8 +23,22 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity<ApiError> handleResourceNotFoundException(ResourceNotFoundException exc) {
         List<String> details = new ArrayList<>();
         details.add(exc.getMessage());
-        ApiError apiError = new ApiError(LocalDateTime.now(), HttpStatus.BAD_REQUEST, "Resource not found", details);
+        ApiError apiError = new ApiError(LocalDateTime.now(), HttpStatus.NOT_FOUND, "Resource not found", details);
         return ResponseEntityBuilder.build(apiError);
+    }
+
+    // handleNoHandlerFoundException : triggers when the handler method is invalid
+    @Override
+    protected ResponseEntity<Object> handleNoHandlerFoundException(
+            NoHandlerFoundException ex, HttpHeaders headers, HttpStatusCode status, WebRequest request) {
+
+        List<String> details = new ArrayList<String>();
+        details.add(String.format("Could not find the %s method for URL %s", ex.getHttpMethod(), ex.getRequestURL()));
+
+        ApiError err = new ApiError(LocalDateTime.now(), HttpStatus.BAD_REQUEST, "Method Not Found", details);
+
+        return ResponseEntity.badRequest().body(err);
+
     }
 
 }
